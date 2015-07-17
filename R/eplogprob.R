db34520254c8a4a21a69bcc351cfb596fcc650de
merliseclyde <clyde@stat.duke.edu>
2015-07-17 11:34:41 -0400

    provided warning if model is not full rank as dimenion of vector is reduced and not equal to p

--- R/eplogprob.R
+++ R/eplogprob.R
@@ -1,9 +1,15 @@
+#' @export
+#'
 eplogprob = function(lm.obj, thresh=.5, max = 0.99, int=TRUE) {
-   pval = summary(lm.obj)$coefficients[,4]
-   prob = 1/(1 - exp(1)*pval*log(pval))
-   prob[pval > 1/exp(1)] = thresh
-   prob[prob > max] = max
-   if (int) prob[1] = 1.0
+    pval = summary(lm.obj)$coefficients[,4]
+    prob = 1/(1 - exp(1)*pval*log(pval))
+    prob[pval > 1/exp(1)] = thresh
+    prob[prob > max] = max
+    if (int) prob[1] = 1.0
+    if (any(is.na(prob))) {
+        warning("Model is not full rank, do not use eplogp approximation to start sampling\n")
+    }                    
 return(prob)
- }
+}
+
 
