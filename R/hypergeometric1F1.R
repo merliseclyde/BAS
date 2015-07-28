e70564ec9ef3027a96c3181c583744134505d976
merliseclyde <clyde@stat.duke.edu>
2015-07-28 11:45:54 -0400

    R function to call Cephes

--- R/hypergeometric1F1.R
+++ R/hypergeometric1F1.R
@@ -1,10 +1,6 @@
 hypergeometric1F1 = function(a,b,z, method="Cephes", log=TRUE) {
   out = 1.0
-  if (a < b  | b < 0) {
-    warning("Must have a > and b > 0 in 1F1 function for integral to converge")
-    return(Inf)}
-else{
-    ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(z), out=as.numeric(out), PACKAGE="BAS")$out
+  ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(z), out=as.numeric(out, as.integer(length(a)), PACKAGE="BAS")$out
 }
   return(ans)
 }
