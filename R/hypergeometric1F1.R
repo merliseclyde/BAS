fe0a238e2942b4bec4ee1bb504f9ead753c07e3f
merliseclyde <clyde@stat.duke.edu>
2015-07-28 11:46:27 -0400

    R function to call Cephes

--- R/hypergeometric1F1.R
+++ R/hypergeometric1F1.R
@@ -1,6 +1,5 @@
 hypergeometric1F1 = function(a,b,z, method="Cephes", log=TRUE) {
   out = 1.0
   ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(z), out=as.numeric(out, as.integer(length(a)), PACKAGE="BAS")$out
-}
   return(ans)
 }
