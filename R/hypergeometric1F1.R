d78bb079f8aed965999cf629e1072c1ed4fd337c
merliseclyde <clyde@stat.duke.edu>
2015-09-08 23:31:32 -0400

    added switches for computing 1F1

--- R/hypergeometric1F1.R
+++ R/hypergeometric1F1.R
@@ -1,7 +1,7 @@
-hypergeometric1F1 = function(a,b,z, laplace=TRUE, log=TRUE) {
+hypergeometric1F1 = function(a,b,c, laplace=TRUE, log=TRUE) {
 
     n = length(a);
     out = rep(0, n);
-    ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(z), out=as.numeric(out), as.integer(n), as.integer(rep(laplace, n)), PACKAGE="BAS")$out
+    ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(c), out=as.numeric(out), as.integer(n), as.integer(rep(laplace, n)), PACKAGE="BAS")$out
     return(ans)
 }
