a47a59614fd73ebc5ac4493a5272a698f2fe6fbc
merliseclyde <clyde@stat.duke.edu>
2015-10-28 20:29:48 -0400

    made default to use Cephes, and fix problem where log=T was ignored

--- R/hypergeometric1F1.R
+++ R/hypergeometric1F1.R
@@ -1,7 +1,8 @@
-hypergeometric1F1 = function(a,b,c, laplace=TRUE, log=TRUE) {
+hypergeometric1F1 = function(a,b,c, laplace=FALSE, log=TRUE) {
 
     n = length(a);
     out = rep(0, n);
     ans = .C("hypergeometric1F1", as.numeric(a), as.numeric(b), as.numeric(c), out=as.numeric(out), as.integer(n), as.integer(rep(laplace, n)), PACKAGE="BAS")$out
+    if (!log) ans = exp(ans)
     return(ans)
 }
