9cdcd6e82aee69a7039f24c61d0c1e53129ac191
merliseclyde <clyde@stat.duke.edu>
2016-03-09 22:10:30 -0500

    defined truncated poisson and beta-binomail priors

--- R/modelpriors.R
+++ R/modelpriors.R
@@ -13,3 +13,16 @@ beta.binomial=function(alpha=1.0, beta=1.0) {
     structure(list(family="Beta-Binomial", hyper.parameters=c(alpha, beta)),
               class="prior")
  }
+
+
+tr.beta.binomial=function(alpha=1.0, beta=1.0, trunc) {
+    structure(list(family="Trunc-Beta-Binomial", hyper.parameters=c(alpha, beta, trunc)),
+              class="prior")
+ }
+
+
+
+tr.poisson=function(lambda, trunc) {
+    structure(list(family="Trunc-Poisson", hyper.parameters=c(lambda, trunc)),
+              class="prior")
+ }
