bef98f1d5e55f69afaff9c04645ab95f90e40b6e
merliseclyde <clyde@stat.duke.edu>
2018-07-24 14:51:40 -0400

    add tests

---
+++ tests/JASP-tests.R
@@ -0,0 +1,14 @@
+loc = system.file("testdata", package="BAS")
+d = read.csv(paste(loc, "JASP-testdata.csv", sep="/"))
+
+simpleFormula = as.formula("contNormal ~ contGamma + contcor1 + contGamma * contcor1 ")
+
+basObj = BAS::bas.lm(simpleFormula,
+                      data = d,
+                      alpha = 0.125316,
+                      prior = "JZS", include.always=as.formula("contNormal ~ contcor1"),
+                      weights = d$facFifty)
+
+image(basObj, rotate=FALSE)
+image(basObj, rotate=FALSE, drop.always.included=TRUE)
+basObj$include.always
