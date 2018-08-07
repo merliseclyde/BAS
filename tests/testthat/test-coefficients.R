e3f1ada0f2de1514d4e7762342d15c318a49ca72
merliseclyde <clyde@stat.duke.edu>
2018-08-07 16:07:23 -0400

    added test for plot.coef.bas

--- tests/testthat/test-coefficients.R
+++ tests/testthat/test-coefficients.R
@@ -12,3 +12,13 @@ test_that("coefficients", {
   expect_equal(coefficients(hald_gprior, estimator='HPM')$postmean,
                coefficients(hald_gprior, estimator='BMA', n.models=1)$postmean)
 })
+
+test_that("plot posterior coefficients", {
+  data(UScrime, package="MASS")
+  UScrime[,-2] <- log(UScrime[,-2])
+  crime_bic <- bas.lm(y ~ ., data=UScrime, n.models=2^10, prior="BIC")
+  crime_coef <- coefficients(crime_bic)
+  expect_null(plot(crime_coef, subset=2:4, ask=TRUE))
+  expect_null(plot(crime_coef, ask=FALSE))
+})
+
