a01a4dd889e51a97c9545e3bb07a11d97b58cca4
merliseclyde <clyde@stat.duke.edu>
2018-08-06 23:41:58 -0400

    fixed stray char

--- tests/testthat/test-image.bas.R
+++ tests/testthat/test-image.bas.R
@@ -4,11 +4,9 @@ test_that("test image plots", {
  hald.ZSprior =  bas.lm(Y~ ., data=Hald,  prior="ZS-null")
  expect_null(image(hald.ZSprior, drop.always.included=TRUE))
  expect_null(image(hald.ZSprior, drop.always.included=FALSE))
- expect_null(image(hald.ZSprior, rotate=TRUE))
+ expect_null(image(hald.ZSprior, rotate=FALSE))
  expect_null(image(hald.ZSprior, prob=FALSE))
  expect_null(image(hald.ZSprior, intensity=FALSE))
  expect_null(image(hald.ZSprior, intensity=TRUE, color="blackandwhite"))
+})
 
-  #'
-
-}
