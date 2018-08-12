a2c9c4fa745977fc0d7827fadb3388f920311053
merliseclyde <clyde@stat.duke.edu>
2018-08-11 21:55:31 -0400

    added test for predict.bas.glm - need to add issues for errors encountered!

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,6 +1,6 @@
 context("predict.R")
 
-test_that("predict.bas", {
+test_that("predict.bas.lm", {
   data("Hald")
   hald_gprior <- bas.lm(Y ~ ., data = Hald, alpha = 13, prior = "g-prior")
   hald_pred <- predict(hald_gprior, newdata = Hald, estimator = "BPM",
@@ -9,4 +9,27 @@ test_that("predict.bas", {
   hald_pred <- predict(hald_gprior, estimator = "BMA",
                        se.fit = TRUE)
   expect_null(plot(confint(hald_pred)))
+})
+
+test_that("predict.bas.glm", {
+  data("Pima.tr", package="MASS")
+  data("Pima.te", package="MASS")
+  pima_gprior <- bas.glm(type ~ ., data = Pima.tr,
+                         betaprior = g.prior(g=as.numeric(nrow(Pima.tr))),
+                         family=binomial())
+  pima_pred <- predict(pima_gprior,
+                       estimator = "HPM",
+                       se.fit = FALSE)
+
+  # should not error
+  expect_error(predict(pima_gprior,
+                       estimator = "HPM",
+                       se.fit = TRUE))
+#  expect_null(plot(confint(pima_pred, parm = "mean")))
+#  should not error
+  expect_error( predict(hald_gprior, newdata=Pima.te, estimator = "HPM",
+                       se.fit = TRUE))
+  expect_error( predict(hald_gprior, data=Pima.te, estimator = "HPM",
+                        se.fit = TRUE))
+  expect_null(plot(confint(pima_pred)))
 })
