c17649ccd8bb93f06d3c0a49a0c104e0b0097b5b
merliseclyde <clyde@stat.duke.edu>
2018-10-24 00:11:20 -0400

    drop checks for windoze - R not supported

--- tests/testthat/test-bas-lm.R
+++ tests/testthat/test-bas-lm.R
@@ -233,62 +233,7 @@ test_that("interactions", {
                bas_hald$n.models)
  })
 
-test_that("check non-full rank", {
-  loc <- system.file("testdata", package = "BAS")
-  d <- read.csv(paste(loc, "JASP-testdata.csv", sep = "/"))
 
-  fullModelFormula <- as.formula("contNormal ~  contGamma * contExpon + contGamma * contcor1 + contExpon * contcor1")
-
-  bas.lm(fullModelFormula,
-         data = d,
-         alpha = 0.125316,
-         prior = "JZS",
-         weights = facFifty, force.heredity = FALSE, pivot = F
-  )
-  expect_warning(bas.lm(fullModelFormula,
-    data = d,
-    alpha = 0.125316,
-    prior = "JZS",
-    weights = facFifty, force.heredity = FALSE, pivot = F
-  ))
-  expect_error(eplogprob(lm(fullModelFormula, data = d)))
-  basObj.eplogp <- bas.lm(fullModelFormula,
-    data = d,
-    alpha = 0.125316, initprobs = "marg-eplogp",
-    prior = "JZS", method = "deterministic", pivot = T,
-    modelprior = uniform(),
-    weights = facFifty, force.heredity = FALSE
-  )
-  basObj.det <- bas.lm(fullModelFormula,
-    data = d,
-    alpha = 0.125316,
-    modelprior = uniform(),
-    prior = "JZS", method = "deterministic", pivot = T,
-    weights = facFifty, force.heredity = FALSE
-  )
-  basObj <- bas.lm(fullModelFormula,
-    data = d,
-    alpha = 0.125316, modelprior = uniform(),
-    prior = "JZS", method = "BAS", pivot = T,
-    weights = facFifty, force.heredity = FALSE
-  )
-  expect_equal(0, sum(is.na(basObj.det$postprobs)))
-  expect_equal(basObj.eplogp$probne0, basObj.det$probne0)
-  expect_equal(basObj.det$probne0, basObj$probne0)
-  expect_equal(basObj.eplogp$probne0, basObj$probne0)
-
-  basObj.EBL <- bas.lm(fullModelFormula,
-    data = d,
-    alpha = 0.125316, initprobs = "marg-eplogp",
-    prior = "EB-local", method = "deterministic", pivot = T,
-    modelprior = uniform(),
-    weights = facFifty, force.heredity = FALSE
-  )
-  basObj.up <- update(basObj.eplogp, newprior = "EB-local")
-  expect_equal(basObj.EBL$postprobs, basObj.up$postprobs,
-               tolerance=.001)
-  # follow up
-})
 
 # add more testing for update.bas
 #
@@ -306,4 +251,61 @@ test_that("as.matrix tools", {
   m3[, 1] <- 1
   probne0 <- t(m3) %*% hald_bic$postprobs
   expect_equal(as.vector(probne0), hald_bic$probne0)
+})
+
+test_that("check non-full rank", {
+  loc <- system.file("testdata", package = "BAS")
+  d <- read.csv(paste(loc, "JASP-testdata.csv", sep = "/"))
+
+  fullModelFormula <- as.formula("contNormal ~  contGamma * contExpon + contGamma * contcor1 + contExpon * contcor1")
+
+  bas.lm(fullModelFormula,
+         data = d,
+         alpha = 0.125316,
+         prior = "JZS",
+         weights = facFifty, force.heredity = FALSE, pivot = F
+  )
+  expect_warning(bas.lm(fullModelFormula,
+                        data = d,
+                        alpha = 0.125316,
+                        prior = "JZS",
+                        weights = facFifty, force.heredity = FALSE, pivot = F
+  ))
+  expect_error(eplogprob(lm(fullModelFormula, data = d)))
+  basObj.eplogp <- bas.lm(fullModelFormula,
+                          data = d,
+                          alpha = 0.125316, initprobs = "marg-eplogp",
+                          prior = "JZS", method = "deterministic", pivot = T,
+                          modelprior = uniform(),
+                          weights = facFifty, force.heredity = FALSE
+  )
+  basObj.det <- bas.lm(fullModelFormula,
+                       data = d,
+                       alpha = 0.125316,
+                       modelprior = uniform(),
+                       prior = "JZS", method = "deterministic", pivot = T,
+                       weights = facFifty, force.heredity = FALSE
+  )
+  basObj <- bas.lm(fullModelFormula,
+                   data = d,
+                   alpha = 0.125316, modelprior = uniform(),
+                   prior = "JZS", method = "BAS", pivot = T,
+                   weights = facFifty, force.heredity = FALSE
+  )
+  expect_equal(0, sum(is.na(basObj.det$postprobs)))
+  expect_equal(basObj.eplogp$probne0, basObj.det$probne0)
+  expect_equal(basObj.det$probne0, basObj$probne0)
+  expect_equal(basObj.eplogp$probne0, basObj$probne0)
+
+  basObj.EBL <- bas.lm(fullModelFormula,
+                       data = d,
+                       alpha = 0.125316, initprobs = "marg-eplogp",
+                       prior = "EB-local", method = "deterministic", pivot = T,
+                       modelprior = uniform(),
+                       weights = facFifty, force.heredity = FALSE
+  )
+  basObj.up <- update(basObj.eplogp, newprior = "EB-local")
+  expect_equal(basObj.EBL$postprobs, basObj.up$postprobs,
+               tolerance=.001)
+  # follow up
 })
