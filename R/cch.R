f4df596cd39c81b4f4cc384ba7ab89ddaeacb0d0
Merlise Clyde <clyde@stat.duke.edu>
2021-12-13 10:36:10 -0500

    added additional documentation

--- R/cch.R
+++ R/cch.R
@@ -4,12 +4,18 @@
 #' a Horn hypergeometric function or Humbert's hypergeometric used in Gordy
 #' (1998) with integral representation:
 #'
-#'  phi_1(a,b,c,x,y) =  (Gamma(c)/Gamma(a) Gamma(a-c)) Int_0^1
+#'  phi_1(a,b,c,x,y) =  [(Gamma(c)/Gamma(a) Gamma(a-c))] Int_0^1
 #' t^(a-1) (1 - t)^(c-a-1) (1 - yt)^(-b) exp(x t) dt
 #' \url{https://en.wikipedia.org/wiki/Humbert_series} Note that Gordy's
 #' arguments for x and y are reversed in the reference above.
 #'
-#' Code for phi1 provided by Gordy.
+#' The original `phi1` function in `BAS` was based on `C` code provided by 
+#' Gordy.  This function returns NA's 
+#' when x is greater than `log(.Machine$double.xmax)/2`.   A more 
+#' stable method for calculating the `phi1` function using R's `integrate` 
+#' was suggested by Daniel Heemann and is now an option whenever $x$ is too 
+#' large.  For calculating Bayes factors that use the `phi1` function we 
+#' recommend using the `log=TRUE` option to compute log Bayes factors.
 #'
 #'
 #' @param a a > 0
@@ -19,18 +25,22 @@
 #' @param y 0 <= y < 1
 #' @param log logical indicating whether to return phi1 on the log scale
 #' @author Merlise Clyde (\email{clyde@@duke.edu})
+#' @author Daniel Heemann (\email{df.heemann@@gmail.com})
 #' @references Gordy 1998
 #' @keywords math
 #' @examples
 #'
 #' # special cases
 #' # phi1(a, b, c, x=0, y) is the same as 2F1(b, a; c, y)
-#' phi1(1, 2, 1.5, 0, 1 / 100)
+#' phi1(1, 2, 1.5, 0, 1 / 100, log=FALSE)
 #' hypergeometric2F1(2, 1, 1.5, 1 / 100, log = FALSE)
 #'
 #' # phi1(a,0,c,x,y) is the same as 1F1(a,c,x)
 #' phi1(1, 0, 1.5, 3, 1 / 100)
 #' hypergeometric1F1(1, 1.5, 3, log = FALSE)
+#' 
+#' # use direct integration
+#' phi1(1, 2, 1.5, 1000, 0, log=TRUE)
 #' @rdname phi1
 #' @family special functions
 #' @export
@@ -38,7 +48,7 @@
 #'
 phi1 <- function(a, b, c, x, y, log=FALSE) {
   #  phi_1(a,b,c,x,y) =  (Gamma(c)/Gamma(a) Gamma(a-c)) Int_0^1
-  # t^(a-1) (1 - t)^(c-a-1) (1 - yt)^(-b) exp(x t) dt
+  # t^(a-1) (1 - t)^(c-a-1) (1 - y t)^(-b) exp(x t) dt
   na <- length(a)
   nb <- length(b)
   nc <- length(c)
