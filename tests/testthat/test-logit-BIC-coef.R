8fcded7a53805550762c0467af0e6763febb4729
Merlise Clyde <clyde@stat.duke.edu>
2024-03-30 15:00:37 -0400

    test for error reported by Marcin Błażejowski in BAS 1.6.4

---
+++ tests/testthat/test-logit-BIC-coef.R
@@ -0,0 +1,17 @@
+test_that("coef in logig glm", {
+  Nrep = 1000
+  
+  # We load dataset
+  data("birthwt", package = "MASS")
+  
+  birthwt$race <- as.factor(birthwt$race)
+  
+  # bas.glm
+  set.seed(1000000)
+  birthwt.bas.glm <- bas.glm(low ~ . -bwt, data=birthwt,
+                             family=binomial(link = "logit"),
+                             method="MCMC",
+                             MCMC.iterations=Nrep,
+                             laplace=TRUE)
+  expect_no_error(coef(birthwt.bas.glm))
+})
