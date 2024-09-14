5edaf8ca424854c3131178f3d8f9ea112b79fbd7
merliseclyde <clyde@duke.edu>
2024-09-14 18:24:08 -0400

    update run_checks file documentation

--- run_checks.R
+++ run_checks.R
@@ -6,8 +6,6 @@ git checkout main
 # merge changes from devel into main
 git merge devel
 
-# create checklist
-usethis::use_release_issue()
 
 # Check current CRAN check results
 usethis::use_release_issue()
@@ -19,7 +17,7 @@ devtools::check(remote = TRUE, manual = TRUE)
 
 devtools::install_github("r-lib/revdepcheck")
 
-revdepcheck::revded_reset()
+revdepcheck::revdep_reset()
 revdepcheck::revdep_check(num_workers = 4)
 revdepcheck::revdep_report_cran()
 
@@ -38,6 +36,6 @@ ch$update()
 
 
 # to submit to CRAN
-usethis::use_release_issue()
+usethis::use_version('patch')
 
 devtools::submit_cran()
