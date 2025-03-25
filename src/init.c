68d750ec708a3b239e77e94caf1f5b1787b0dc93
merliseclyde <clyde@duke.edu>
2025-03-25 18:46:51 -0400

    add src/mcmc.c and function mcmc (for lm) to .Rbuildignore and comment out mcmc in init.c

--- src/init.c
+++ src/init.c
@@ -26,7 +26,7 @@ extern SEXP glm_mcmc_grow(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, 
 extern SEXP glm_mcmcbas(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP glm_sampleworep(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP deterministic(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP mcmc(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+// extern SEXP mcmc(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP mcmc_grow(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP amcmc(SEXP, SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP mcmcbas(SEXP,SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
@@ -51,7 +51,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"glm_mcmc_grow",     (DL_FUNC) &glm_mcmc_grow,     18}, 
   {"glm_mcmcbas",       (DL_FUNC) &glm_mcmcbas,       17},
   {"glm_sampleworep",   (DL_FUNC) &glm_sampleworep,   15},
-  {"mcmc",              (DL_FUNC) &mcmc,              20},
+//  {"mcmc",              (DL_FUNC) &mcmc,              20},
   {"mcmc_grow",         (DL_FUNC) &mcmc_grow,         21},
   {"amcmc",             (DL_FUNC) &amcmc,             21},
   {"deterministic",     (DL_FUNC) &deterministic,     11},
