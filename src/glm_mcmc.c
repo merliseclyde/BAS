90c28f6dbe777c8a85d0386fb63a538e4de25cdd
merliseclyde <clyde@duke.edu>
2025-12-11 15:45:49 -0500

    check that nmodels is correct

--- src/glm_mcmc.c
+++ src/glm_mcmc.c
@@ -27,6 +27,9 @@ SEXP glm_mcmc(SEXP Y, SEXP X, SEXP Roffset, SEXP Rweights,
   
 	int nProtected = 0;
 	int nModels=LENGTH(Rmodeldim);
+	
+	Rprintf("nModels to sample is %d\n", nModels);
+	
 	/*
 	SEXP ANS = PROTECT(allocVector(VECSXP, 17)); ++nProtected;
 	SEXP ANS_names = PROTECT(allocVector(STRSXP, 17)); ++nProtected;
@@ -285,7 +288,7 @@ SEXP glm_mcmc(SEXP Y, SEXP X, SEXP Roffset, SEXP Rweights,
 	SET_VECTOR_ELT(ANS, 0, Rprobs);
 	SET_VECTOR_ELT(ANS, 13, MCMCprobs);
 	
-  //	Rprintf("Decreasing nModels %d to number of unique models accepted %d \n", nModels, nUnique);
+  Rprintf("Decreasing nModels %d to number of unique models accepted %d \n", nModels, nUnique);
 	if (nUnique < nModels) {
 	  SET_VECTOR_ELT(ANS, 1, resizeVector(modelspace, nUnique));
 	  SET_VECTOR_ELT(ANS, 2, resizeVector(logmarg, nUnique));
