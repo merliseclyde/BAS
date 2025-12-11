dc9f166121f18bfd4246c54f3727a0b53d640640
merliseclyde <clyde@duke.edu>
2025-12-11 16:14:54 -0500

    pass in initial models as opposed to vector

--- src/glm_sampleworep.c
+++ src/glm_sampleworep.c
@@ -8,15 +8,16 @@
 
 
 SEXP glm_sampleworep(SEXP Y, SEXP X, SEXP Roffset, SEXP Rweights,
-		     SEXP Rprobinit, SEXP Rmodeldim,
+		     SEXP Rprobinit, SEXP RnModels,
 		     SEXP modelprior, SEXP betaprior,SEXP Rbestmodel,  SEXP plocal,
 		     SEXP family, SEXP Rcontrol,
 		     SEXP Rupdate, SEXP Rlaplace, SEXP Rparents) {
 
   int nProtected = 0;
-  int nModels=LENGTH(Rmodeldim);
+  int nModels = INTEGER(RnModels)[0];  // initial guess on number of models to return
+  int nUnique = nModels;
 
-	//  Rprintf("Allocating Space for %d Models\n", nModels) ;
+  Rprintf("Allocating Space for %d Models\n", nModels) ;
 
 	SEXP ANS = PROTECT(allocVector(VECSXP, 15)); ++nProtected;
 	SEXP ANS_names = PROTECT(allocVector(STRSXP, 15)); ++nProtected;
@@ -115,7 +116,6 @@ SEXP glm_sampleworep(SEXP Y, SEXP X, SEXP Roffset, SEXP Rweights,
 
 	//get dimsensions of all variables
 	int p = INTEGER(getAttrib(X,R_DimSymbol))[1];
-	int nUnique = nModels;
 
 	int update = INTEGER(Rupdate)[0];
 	double eps = DBL_EPSILON;
@@ -173,7 +173,7 @@ SEXP glm_sampleworep(SEXP Y, SEXP X, SEXP Roffset, SEXP Rweights,
 	int *modelwork= ivecalloc(p);
 
 	// sample models
-	for (m = 1;  m <nUnique  && lessThanOne(pigamma[0]); m++) {
+	for (m = 1;  m < nUnique  && lessThanOne(pigamma[0]); m++) {
 	  INTEGER(modeldim)[m] = 0.0;
 		for (i = n; i < p; i++)  {
 			INTEGER(modeldim)[m]  +=  model[vars[i].index];
